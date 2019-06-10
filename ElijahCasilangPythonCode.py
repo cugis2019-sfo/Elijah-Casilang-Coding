8136ae57914a3edbe99463cfc2daf85d6b90e457
ElijahrC <51681228+ElijahrC@users.noreply.github.com>
2019-06-10 16:52:17 -0700

    Add files via upload

---
+++ ElijahCasilangPythonCode.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+5/8 #divison
+print("You are in San Francisco the number","1",)
+
+5*2 #multiplication
+
+5-2 #subtraction
+
+5**2 #exponent
+
+(8/9)*3 #multiplication
+
+#writing code to sum any two numbers
+
+  
+def areaoftri (b*h)/2:
+    
+    print ("the area is", areaoftri, "units squared")
+    
+areaoftri(12,7)
