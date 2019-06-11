b2448c5887664cf07cd372dda7ae59fafaf71dc2
ElijahrC <51681228+ElijahrC@users.noreply.github.com>
2019-06-11 10:42:07 -0700

    Add files via upload

---
+++ ElijahCasilangPythonCodeDay2.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+5/8 #divison
+print("You are in San Francisco the number","1",)
+
+5*2 #multiplication
+
+5-2 #subtraction
+
+5**2 #exponent
+
+(8/9)*3 #multiplication
+
+#writing code to sum any two numbers
+
+def area (b,h):
+    area = (b*h)/2
+    
+    print ("area is",area,"unit squared")
+    
+area(5,6)
+
+#writing code in order to greet someone by name
+
+def greeting(a):
+    print("Hello how are u you today", a)
+    
+greeting("Elijah")
