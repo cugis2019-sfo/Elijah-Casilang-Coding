8c8298dbd27f160edb5575bff625f5910db2b766
ElijahrC <51681228+ElijahrC@users.noreply.github.com>
2019-06-11 15:28:13 -0700

    Add files via upload

---
+++ ElijahCasilangPythonCodeDay2Part2.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+5/8 #divison
+print("You are in San Francisco the number","1",)
+
+5*2 #multiplication
+
+5-2 #subtraction
+
+5**2 #exponent
+
+(8/9)*3 #multiplication
+
+#writing code to sum any two numbers
+
+def area (b,h):
+    area = (b*h)/2
+    
+    print ("area is",area,"unit squared")
+    
+area(5,6)
+
+#writing code in order to greet someone by name
+
+def greeting(a):
+    print("Hello how are u you today", a)
+    
+greeting("Elijah")
+
+
+
+import math
+dir(math)
+
+pi
+math.pi
+math.e
+
+math.sqrt(2)
+math.lo
+
+math.factorial(1730)
+math.pow(3,10)
+math.remainder(4,6)
+
+
+
+#CubicRoot
+
+def cubicroot(a):
+    cubicroot = a
+    print(math.pow(a,(1/3)))
+    
+cubicroot(15)
+
+#codetoperformanaction
+
+def cubicroot(c):
+    cubicroot= c
+    print(math.pow(c,(1/3)))
+    
+cubicroot(t)
+
+def user():
+    print("please select a number for t")
+    
+user()
+cubicroot(t)
